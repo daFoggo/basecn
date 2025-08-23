8cf6fd318b057dde59f046411ccb54a39bd80679
b23dccc055 <ntgiang141105@gmail.com>
2025-08-23 09:56:39 +0700

    update global css

--- components/common/page-heading.tsx
+++ components/common/page-heading.tsx
@@ -2,45 +2,50 @@ import type { ReactNode } from "react";
 import { cn } from "@/lib/utils/tailwind";
 
 interface IPageHeadingProps {
-    title?: string;
-    subtitle?: string;
-    customTitleComponent?: ReactNode;
-    rightSections?: ReactNode;
-    titleClassName?: string;
-    subtitleClassName?: string;
-    rightSectionsClassName?: string;
-    className?: string;
+	title?: string;
+	subtitle?: string;
+	customTitleComponent?: ReactNode;
+	rightSections?: ReactNode;
+	titleClassName?: string;
+	subtitleClassName?: string;
+	rightSectionsClassName?: string;
+	className?: string;
 }
 
 const PageHeading = ({
-    title,
-    subtitle,
-    customTitleComponent,
-    rightSections,
-    titleClassName,
-    subtitleClassName,
-    rightSectionsClassName,
-    className,
+	title,
+	subtitle,
+	customTitleComponent,
+	rightSections,
+	titleClassName,
+	subtitleClassName,
+	rightSectionsClassName,
+	className,
 }: IPageHeadingProps) => {
-    return (
-        <div
-            className={cn("flex justify-between items-start gap-4 w-full", className)}
-        >
-            <div className="flex flex-col items-start gap-1">
-                {customTitleComponent || (
-                    <p className={cn("font-bold text-2xl", titleClassName)}>{title}</p>
-                )}
-                {subtitle && (
-                    <p className={cn("text-muted-foreground text-sm", subtitleClassName)}>
-                        {subtitle}
-                    </p>
-                )}
-            </div>
-            {rightSections && (
-                <div className={cn("", rightSectionsClassName)}>{rightSections}</div>
-            )}
-        </div>
-    );
+	return (
+		<div
+			className={cn("flex justify-between items-start gap-4 w-full", className)}
+		>
+			<div className="flex flex-col items-start gap-1">
+				{customTitleComponent || (
+					<p className={cn("font-bold text-2xl", titleClassName)}>{title}</p>
+				)}
+				{subtitle && (
+					<p
+						className={cn(
+							"font-medium text-md text-muted-foreground",
+							subtitleClassName,
+						)}
+					>
+						{subtitle}
+					</p>
+				)}
+			</div>
+			{rightSections && (
+				<div className={cn("", rightSectionsClassName)}>{rightSections}</div>
+			)}
+		</div>
+	);
 };
 
 export default PageHeading;
